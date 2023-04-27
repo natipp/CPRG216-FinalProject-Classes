24707a1be9601c59988ed36ae563911190bab303
Jubril Somide <jbsomide@gmail.com>
2023-04-26 23:07:39 -0600

    Added comments to the files

--- Patient.py
+++ Patient.py
@@ -1,3 +1,4 @@
+#Creation of the Patient class
 class Patient:
     def __init__(self, pid, name, disease, gender, age):
         self.pid = pid
@@ -6,6 +7,7 @@ class Patient:
         self.gender = gender
         self.age = age
 
+    # Getters and Setters
     def get_pid(self):
         return self.pid
 
@@ -36,5 +38,6 @@ class Patient:
     def set_age(self, new_age):
         self.age = new_age
 
+    # Method to print the object
     def __str__(self):
         return f"{self.pid}_{self.name}_{self.disease}_{self.gender}_{self.age}"
--- PatientManager.py
+++ PatientManager.py
@@ -1,14 +1,17 @@
+#Importing Patient class from Patient.py
 from Patient import Patient
 
-
+#Creation of the PatientManager class
 class PatientManager:
     def __init__(self):
         self.patients = []
         self.read_patients_file()
 
+    # Method to format the patient information for the file
     def format_patient_Info_for_file(self, patient):
         return f"{patient.pid}_{patient.name}_{patient.disease}_{patient.gender}_{patient.age}"
 
+    # Method to save the patient information to the file
     def enter_patient_info(self):
         pid = input("\nEnter Patient id: ")
         name = input("Enter Patient name: ")
@@ -19,6 +22,7 @@ class PatientManager:
 
         return patient
 
+    # Method to read the patient information from the file
     def read_patients_file(self):
         with open("Project Data\patients.txt", "r") as file:
             next(file)
@@ -27,6 +31,7 @@ class PatientManager:
                 patient = Patient(pid, name, disease, gender, age)
                 self.patients.append(patient)
 
+    # Method to search for a patient by their id
     def search_patient_by_Id(self):
         pid = input("\nEnter the Patient Id: ")
 
@@ -37,10 +42,12 @@ class PatientManager:
 
         print("Can't find the Patient with the same id on the system")
 
+    #Method to display the patient information
     def display_patient_info(self, patient):
         print(f"{'ID':<6}{'Name':<20}{'Disease':<15}{'Gender':<15}{'Age':<6}\n")
         print(f"{patient.get_pid():<6}{patient.get_name():<20}{patient.get_disease():<15}{patient.get_gender():<15}{patient.get_age():<6}")
 
+    # Method to edir the patient information
     def edit_patient_info_by_id(self):
         patient_id = input(
             "\nPlease enter the id of the Patient that you want to edit their information: ")
@@ -70,16 +77,19 @@ class PatientManager:
         else:
             print(f"Cannot find the patient {patient_id}")
 
+    #Method to display the list of patients
     def display_patients_list(self):
         print(f"{'ID':<6}{'Name':<20}{'Disease':<15}{'Gender':<15}{'Age':<6}\n")
         for patient in self.patients:
             print(f"{patient.get_pid():<6}{patient.get_name():<20}{patient.get_disease():<15}{patient.get_gender():<15}{patient.get_age():<6}\n")
 
+    # Method to write the list of patients to the file
     def write_list_of_patients_to_file(self):
         with open("Project Data\patients.txt", "w") as file:
             for patient in self.patients:
                 file.write(self.format_patient_Info_for_file(patient) + "\n")
 
+    #Method to add a patient to the list
     def add_patient_to_file(self):
         # Ask user to enter patient info
         new_patient = self.enter_patient_info()
